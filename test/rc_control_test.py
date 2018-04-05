e7dee64d027d04ba4bde2ca61024401a9178cbfa
Oliver Dobner <oliver.dobner@flincteam.org>
2018-04-05 15:29:41 +0200

    updates rc_control_test to python3

--- test/rc_control_test.py
+++ test/rc_control_test.py
@@ -9,7 +9,7 @@ class RCTest(object):
 
     def __init__(self):
         pygame.init()
-        self.ser = serial.Serial('/dev/tty.usbmodem1421', 115200, timeout=1)
+        self.ser = serial.Serial('/dev/tty.usbmodem1411', 115200, timeout=1)
         self.send_inst = True
         self.steer()
 
@@ -23,47 +23,47 @@ class RCTest(object):
                     # complex orders
                     if key_input[pygame.K_UP] and key_input[pygame.K_RIGHT]:
                         print("Forward Right")
-                        self.ser.write(chr(6))
+                        self.ser.write(bytes([6]))
 
                     elif key_input[pygame.K_UP] and key_input[pygame.K_LEFT]:
                         print("Forward Left")
-                        self.ser.write(chr(7))
+                        self.ser.write(bytes([7]))
 
                     elif key_input[pygame.K_DOWN] and key_input[pygame.K_RIGHT]:
                         print("Reverse Right")
-                        self.ser.write(chr(8))
+                        self.ser.write(bytes([8]))
 
                     elif key_input[pygame.K_DOWN] and key_input[pygame.K_LEFT]:
                         print("Reverse Left")
-                        self.ser.write(chr(9))
+                        self.ser.write(bytes([9]))
 
                     # simple orders
                     elif key_input[pygame.K_UP]:
                         print("Forward")
-                        self.ser.write(chr(1))
+                        self.ser.write(bytes([1]))
 
                     elif key_input[pygame.K_DOWN]:
                         print("Reverse")
-                        self.ser.write(chr(2))
+                        self.ser.write(bytes([2]))
 
                     elif key_input[pygame.K_RIGHT]:
                         print("Right")
-                        self.ser.write(chr(3))
+                        self.ser.write(bytes([3]))
 
                     elif key_input[pygame.K_LEFT]:
                         print("Left")
-                        self.ser.write(chr(4))
+                        self.ser.write(bytes([4]))
 
                     # exit
                     elif key_input[pygame.K_x] or key_input[pygame.K_q]:
-                        print 'Exit'
+                        print("Exit")
                         self.send_inst = False
-                        self.ser.write(chr(0))
+                        self.ser.write(bytes([0]))
                         self.ser.close()
                         break
 
                 elif event.type == pygame.KEYUP:
-                    self.ser.write(chr(0))
+                    self.ser.write(bytes([0]))
 
 if __name__ == '__main__':
     RCTest()
