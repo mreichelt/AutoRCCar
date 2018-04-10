29bc827b2c905df3d5491c2b625a5dc811bb55e8
Roman Graef <roman.graef@gmail.com>
2018-04-10 08:56:42 +0200

    Modified `rc_control_test.py` to use `CarControl`

--- test/rc_control_test.py
+++ test/rc_control_test.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
+from serial_util import select_car
+
 __author__ = 'zhengwang'
 
-import serial
 import pygame
 from pygame.locals import *
 
@@ -9,62 +10,64 @@ from pygame.locals import *
 class RCTest(object):
 
     def __init__(self):
-        pygame.init()
-        self.ser = serial.Serial('/dev/tty.usbmodem1411', 115200, timeout=1)
+        self.car = select_car()
         self.send_inst = True
+        pygame.init()
+        print("Control with WASD = combinations")
         self.steer()
 
     def steer(self):
 
         while self.send_inst:
             for event in pygame.event.get():
+                print(event)
                 if event.type == KEYDOWN:
                     key_input = pygame.key.get_pressed()
 
                     # complex orders
                     if key_input[pygame.K_UP] and key_input[pygame.K_RIGHT]:
                         print("Forward Right")
-                        self.ser.write(bytes([6]))
+                        self.car.forward_right()
 
                     elif key_input[pygame.K_UP] and key_input[pygame.K_LEFT]:
                         print("Forward Left")
-                        self.ser.write(bytes([7]))
+                        self.car.forward_left()
 
                     elif key_input[pygame.K_DOWN] and key_input[pygame.K_RIGHT]:
                         print("Reverse Right")
-                        self.ser.write(bytes([8]))
+                        self.car.reverse_right()
 
                     elif key_input[pygame.K_DOWN] and key_input[pygame.K_LEFT]:
                         print("Reverse Left")
-                        self.ser.write(bytes([9]))
+                        self.car.reverse_left()
 
                     # simple orders
                     elif key_input[pygame.K_UP]:
                         print("Forward")
-                        self.ser.write(bytes([1]))
+                        self.car.forward()
 
                     elif key_input[pygame.K_DOWN]:
                         print("Reverse")
-                        self.ser.write(bytes([2]))
+                        self.car.reverse()
 
                     elif key_input[pygame.K_RIGHT]:
                         print("Right")
-                        self.ser.write(bytes([3]))
+                        self.car.right()
 
                     elif key_input[pygame.K_LEFT]:
                         print("Left")
-                        self.ser.write(bytes([4]))
+                        self.car.left()
 
                     # exit
                     elif key_input[pygame.K_x] or key_input[pygame.K_q]:
                         print("Exit")
                         self.send_inst = False
-                        self.ser.write(bytes([0]))
-                        self.ser.close()
+                        self.car.reset_car()
+                        self.car.close()
                         break
 
                 elif event.type == pygame.KEYUP:
-                    self.ser.write(bytes([0]))
+                    self.car.reset_car()
 
 
 if __name__ == '__main__':
