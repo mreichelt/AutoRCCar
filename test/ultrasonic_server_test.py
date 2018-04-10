5195ce4c9353d28b985f4345011c1cb8271e64e3
Roman Graef <roman.graef@gmail.com>
2018-04-10 12:34:09 +0200

    `ultrasonic_server_test` upgraded to python3

--- test/ultrasonic_server_test.py
+++ test/ultrasonic_server_test.py
@@ -1,3 +1,5 @@
+from random_util import get_my_ip
+
 __author__ = 'zhengwang'
 
 import socket
@@ -8,7 +10,8 @@ class SensorStreamingTest(object):
     def __init__(self):
 
         self.server_socket = socket.socket()
-        self.server_socket.bind(('192.168.1.100', 8002))
+        self.server_socket.bind(('0.0.0.0', 8002))
+        print("I am listening on ip, port: ", get_my_ip(), 8002)
         self.server_socket.listen(0)
         self.connection, self.client_address = self.server_socket.accept()
         self.streaming()
@@ -16,19 +19,20 @@ class SensorStreamingTest(object):
     def streaming(self):
 
         try:
-            print "Connection from: ", self.client_address
+            print("Connection from: ", self.client_address)
             start = time.time()
 
             while True:
                 sensor_data = float(self.connection.recv(1024))
-                print "Distance: %0.1f cm" % sensor_data
+                print("Distance: %0.1f cm" % sensor_data)
 
                 # testing for 10 seconds
                 if time.time() - start > 10:
-                    break
+                    pass  # break
         finally:
             self.connection.close()
             self.server_socket.close()
+
 
 if __name__ == '__main__':
     SensorStreamingTest()
