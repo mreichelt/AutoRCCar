f8f2114bd4f56eb4c2c21cb35ec3c7536b3c8398
Roman Graef <roman.graef@gmail.com>
2018-04-18 15:47:23 +0200

    threading bug fixed

--- keras_remake/rc_driver.py
+++ keras_remake/rc_driver.py
@@ -5,12 +5,14 @@ from serial_util import select_car
 
 __author__ = 'zhengwang'
 
+import threading
 import socketserver
 import cv2
 import cv2.ml
 import numpy as np
+import atexit
 
-sensor_data = []
+sensor_data = None
 
 STEER_THRESHOLD = 0.3
 GAS_INTERVAL = 3
@@ -21,9 +23,20 @@ class RCControl(object):
     def __init__(self):
         self.car = select_car()
         self.car.start()
+        self.last_distance = 3000
 
-    def steer(self, prediction, frame):
-        print(end=str(frame))
+    def steer(self, prediction, frame, distance):
+        print(end="%05d: " % frame)
+
+        if self.last_distance >= 25 > distance:
+            self.car.horn()
+        self.last_distance = distance
+
+        if distance < 20:
+            print('stop, because of obstacle')
+            self.car.reset_car()
+            return
+
         if prediction < -STEER_THRESHOLD:
             if frame % GAS_INTERVAL != 0:
                 print('left')
@@ -60,7 +73,7 @@ class SensorDataHandler(socketserver.BaseRequestHandler):
                 self.data = self.request.recv(1024)
                 sensor_data = round(float(self.data), 1)
                 # print "{} sent:".format(self.client_address[0])
-                print(sensor_data)
+                print('Got sensor data:', sensor_data)
         finally:
             print("Connection closed on thread 2")
 
@@ -70,13 +83,14 @@ JPED_END = np.array([0xff, 0xd9], dtype=np.uint8)
 
 
 class VideoStreamHandler(socketserver.StreamRequestHandler):
-    # create neural network
-    model = NeuralNetwork('models/default.h5')
 
-    rc_car = RCControl()
+    def __init__(self, request, client_address, server):
+        self.rc_car = RCControl()
+        atexit.register(self.rc_car.stop)
+        self.model = NeuralNetwork('models/default.h5')
+        super().__init__(request, client_address, server)
 
     def handle(self):
-
         global sensor_data
         stream_bytes = np.array([], dtype=np.uint8)
 
@@ -95,9 +109,12 @@ class VideoStreamHandler(socketserver.StreamRequestHandler):
                         stream_bytes = stream_bytes[last + 2:]
                         gray = cv2.imdecode(jpg, cv2.IMREAD_GRAYSCALE)
 
-                        prediction = self.model.predict_single(gray)
+                        self.model.predict([cv2.imread("training_images/frame01094.jpg", cv2.IMREAD_GRAYSCALE)])
 
-                        self.rc_car.steer(prediction, frame)
+                        prediction = (max(min(self.model.predict_single(gray), 9), 1) - 5) / 4
+
+                        self.rc_car.steer(prediction, frame=frame,
+                                          distance=sensor_data if sensor_data is not None else 3000)
 
                         frame += 1
 
@@ -116,8 +133,18 @@ class ThreadServer(object):
         server = socketserver.TCPServer((host, port), VideoStreamHandler)
         server.serve_forever()
 
+    @staticmethod
+    def sensor_thread(host, port):
+        server = socketserver.TCPServer((host, port), SensorDataHandler)
+        server.serve_forever()
+
     def __init__(self):
-        ThreadServer.server_thread('0.0.0.0', 8000)
+        server_thread = threading.Thread(target=ThreadServer.server_thread, args=('0.0.0.0', 8000))
+        sensor_thread = threading.Thread(target=ThreadServer.sensor_thread, args=('0.0.0.0', 8002))
+        server_thread.start()
+        sensor_thread.start()
+        server_thread.join()
+        sensor_thread.join()
 
 
 if __name__ == '__main__':
